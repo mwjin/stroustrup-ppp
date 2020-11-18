0470559e774ea5c2ffad1f07d9cf34e9ad7b69ab
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-11-18 21:03:44 +0900

    Modify the 'name and age' example as instructed in 65p

---
+++ chap03/name_and_age.cpp
@@ -0,0 +1,11 @@
+# include "../std_lib_facilities.h"
+
+int main() {
+	cout << "Please enter your first name and age\n";
+	string first_name = "???";	// "???" means the first name is unknown.
+	double age = 0.0;			// 0.0 means the age is unknown.
+	cin >> first_name >> age;
+
+	double age_month = age * 12;
+	cout << "Hello, " << first_name << " (age " << age_month << " months)\n";
+}
