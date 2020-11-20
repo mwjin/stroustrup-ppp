c25da6074c45fb50d46852679920036a2b4d3320
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-11-20 10:50:34 +0900

    Retab cpp files

--- chap02/ex01.cpp
+++ chap02/ex01.cpp
@@ -1,8 +1,8 @@
 #include "../std_lib_facilities.h"
-int main()	// C++ programs start by executing the function main 
+int main()  // C++ programs start by executing the function main 
 {
-	cout << "Hello, programming!\n";
-	cout << "Here we go!\n";
-	keep_window_open();			// wait for a character to be entered
-	return 0;
+    cout << "Hello, programming!\n";
+    cout << "Here we go!\n";
+    keep_window_open();         // wait for a character to be entered
+    return 0;
 }
