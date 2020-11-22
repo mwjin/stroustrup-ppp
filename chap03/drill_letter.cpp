ed5531bb73f61fd2fa3824cc41e5668c40861754
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-11-22 19:47:10 +0900

    Do the drill 1

---
+++ chap03/drill_letter.cpp
@@ -0,0 +1,9 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    string first_name;
+    cout << "Enter the name of the person you want to write to: ";
+    cin >> first_name;
+    cout << "Dear, " << first_name << endl;
+}
