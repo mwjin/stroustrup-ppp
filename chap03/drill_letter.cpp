48bf227a08a09253ea54ce5263c11bebef1dcfd4
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-11-22 20:16:10 +0900

    Do the drill 4

--- chap03/drill_letter.cpp
+++ chap03/drill_letter.cpp
@@ -4,12 +4,18 @@ int main()
 {
     string first_name;
     string friend_name;
+    char friend_sex = 0;
+    string friend_obj;
 
-    // Prompts for the variables
+    // Initial variable settings by these prompts
     cout << "Enter the name of the person you want to write to: ";
     cin >> first_name;
     cout << "Enter the name of the friend whose safety you want to ask for: ";
     cin >> friend_name;
+    cout << "Enter the sex of the friend (m (male) or f (female)): ";
+    cin >> friend_sex;
+    friend_obj = (friend_sex == 'm') ? "him" : "her";
+    cout << endl;
 
     // Header
     cout << "Dear, " << first_name << endl;
@@ -22,4 +28,6 @@ int main()
     // Main purpose
     cout << "I would like to ask a question. ";
     cout << "Have you seen " << friend_name << " lately?" << endl;
+    cout << "If you see " << friend_name << " please ask " << friend_obj;
+    cout << " to call me." << endl;
 }
