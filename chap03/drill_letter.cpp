4aab82c8490ea97ec47b647fab5b915e986bdecc
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-11-22 19:56:10 +0900

    Do the drill 2

--- chap03/drill_letter.cpp
+++ chap03/drill_letter.cpp
@@ -3,7 +3,19 @@
 int main()
 {
     string first_name;
+
+    // Prompts for the variables
     cout << "Enter the name of the person you want to write to: ";
     cin >> first_name;
+
+    // Header
     cout << "Dear, " << first_name << endl;
+    cout << endl;
+    
+    // Introduction (greeting)
+    cout << "  How have you been? " << "Hope you are doing well." << endl;
+    cout << endl;
+
+    // Main purpose
+    cout << "I would like to ask a question.";
 }
