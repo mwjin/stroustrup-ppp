3eed9c4b293cb5c116a670e3f7461f180c21299c
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-12-08 11:35:07 +0900

    Enter a prompt to get a writer's name, print it at the end of the letter and clean up code a little bit

--- chap03/drill_letter.cpp
+++ chap03/drill_letter.cpp
@@ -2,49 +2,54 @@
 
 int main()
 {
-    string first_name;
+    string writer_name;
+    string rcpt_name;
+    int rcpt_age = 0;
     string friend_name;
     char friend_sex = 0;
     string friend_obj;
-    int age = 0;
 
-    // Initial variable settings by these prompts
+    /* Initial variable settings by these prompts */
+    cout << "Enter your name: ";
+    cin >> writer_name;
+
     cout << "Enter the name of the person you want to write to: ";
-    cin >> first_name;
+    cin >> rcpt_name;
+    cout << "Enter the age of the recipient: ";
+    cin >> rcpt_age;
+    
+    // Validity check for the recipient's age
+    if (rcpt_age <= 0 || rcpt_age >= 110)
+        error("you're kidding!");
+    
     cout << "Enter the name of the friend whose safety you want to ask for: ";
     cin >> friend_name;
     cout << "Enter the sex of the friend (m (male) or f (female)): ";
     cin >> friend_sex;
     friend_obj = (friend_sex == 'm') ? "him" : "her";
-    cout << "Enter the age of the recipient: ";
-    cin >> age;
     
-    // Validity check for the recipient's age
-    if (age <= 0 || age >= 110)
-        error("you're kidding!");
-    
-    // Header
+    /* Header */
     cout << endl;
-    cout << "Dear, " << first_name << endl;
+    cout << "Dear, " << rcpt_name << endl;
     cout << endl;
     
-    // Introduction (greeting)
+    /* Introduction (greeting) */
     cout << "  How have you been? " << "Hope you are doing well." << endl;
     cout << endl;
 
-    // Main text
+    /* Main text */
     cout << "I would like to ask a question. ";
     cout << "Have you seen " << friend_name << " lately? ";
     cout << "If you see " << friend_name << " please ask " << friend_obj;
     cout << " to call me." << endl;
     cout << "I hear you just had a birthday and you are "; 
-    cout << age << " years old.";
+    cout << rcpt_age << " years old.";
 
-    if (age < 12)
-        cout << " Next year you will be " << age + 1 << ".";
-    else if (age == 17)
+    if (rcpt_age < 12)
+        cout << " Next year you will be " << rcpt_age + 1 << ".";
+    else if (rcpt_age == 17)
         cout << " Next year you will be able to vote.";
-    else if (age > 70)
+    else if (rcpt_age > 70)
         cout << " I hope you are enjoying retirement.";
 
     cout << endl;
@@ -53,9 +58,9 @@ int main()
     cout << "Next time I will be right with you. ";
     cout << "Hope to see you soon!" << endl;
 
-    // Wrap up
+    /* Wrap up */
     cout << endl;
-    cout << "Yours sincerely," << endl;
+    cout << "Yours sincerely,";
     cout << endl << endl;  // Blank lines for a signature
-    cout << first_name << endl;
+    cout << writer_name << endl << endl;
 }
