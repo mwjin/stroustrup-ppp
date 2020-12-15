6df8149f6fd7e631760728f827d6a6c8f1b6e449
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-12-15 23:24:13 +0900

    Improve the exercise 11 of the chapter 3: Change the outputs according to the number of coins.

--- chap03/ex11_coin_sum.cpp
+++ chap03/ex11_coin_sum.cpp
@@ -13,8 +13,8 @@ int main() {
     cout << "How many pennies do you have? ";
     cin >> penny_cnt;
     if (penny_cnt < 0) {
-        cout << "The number of the coins is negative. Are you serious?" 
-        << endl;
+        cout << "The number of the coins is negative. Are you serious?";
+        cout << endl;
         return 0;
     }
     total_cent += penny_cnt;
@@ -22,8 +22,8 @@ int main() {
     cout << "How many nickels do you have? ";
     cin >> nickel_cnt;
     if (nickel_cnt < 0) {
-        cout << "The number of the coins is negative. Are you serious?" 
-        << endl;
+        cout << "The number of the coins is negative. Are you serious?";
+        cout << endl;
         return 0;
     }
     total_cent += nickel_cnt * 5;
@@ -31,8 +31,8 @@ int main() {
     cout << "How many dimes do you have? ";
     cin >> dime_cnt;
     if (dime_cnt < 0) {
-        cout << "The number of the coins is negative. Are you serious?" 
-        << endl;
+        cout << "The number of the coins is negative. Are you serious?"; 
+        cout << endl;
         return 0;
     }
     total_cent += dime_cnt * 10;
@@ -40,8 +40,8 @@ int main() {
     cout << "How many quarters do you have? ";
     cin >> quarter_cnt;
     if (quarter_cnt < 0) {
-        cout << "The number of the coins is negative. Are you serious?" 
-        << endl;
+        cout << "The number of the coins is negative. Are you serious?";
+        cout << endl;
         return 0;
     }
     total_cent += quarter_cnt * 25;
@@ -49,8 +49,8 @@ int main() {
     cout << "How many half dollars do you have? ";
     cin >> half_dollar_cnt;
     if (half_dollar_cnt < 0) {
-        cout << "The number of the coins is negative. Are you serious?" 
-        << endl;
+        cout << "The number of the coins is negative. Are you serious?";
+        cout << endl;
         return 0;
     }
     total_cent += half_dollar_cnt * 50;
@@ -58,20 +58,62 @@ int main() {
     cout << "How many one-dollars do you have? ";
     cin >> one_dollar_cnt;
     if (one_dollar_cnt < 0) {
-        cout << "The number of the coins is negative. Are you serious?" 
-        << endl;
+        cout << "The number of the coins is negative. Are you serious?";
+        cout << endl;
         return 0;
     }
     total_cent += one_dollar_cnt * 100;
     cout << endl;
 
     // Outputs
-    cout << "You have " << penny_cnt << " pennies." << endl; 
-    cout << "You have " << nickel_cnt << " nickels." << endl; 
-    cout << "You have " << dime_cnt << " dimes." << endl; 
-    cout << "You have " << quarter_cnt << " quarters." << endl; 
-    cout << "You have " << half_dollar_cnt << " half dollars." << endl; 
-    cout << "You have " << one_dollar_cnt << " one dollars." << endl;
-    cout << "The value of all of your coins is " << total_cent 
-    << " cents" << endl;
+    if (penny_cnt > 0) {
+        cout << "You have " << penny_cnt;
+        if (penny_cnt == 1)
+            cout << " penny.";
+        else
+            cout << " pennies.";
+        cout << endl;
+    }
+    if (nickel_cnt > 0) {
+        cout << "You have " << nickel_cnt;
+        if (nickel_cnt == 1)
+            cout << " nickel.";
+        else
+            cout << " nickels.";
+        cout << endl;
+    }
+    if (dime_cnt > 0) {
+        cout << "You have " << dime_cnt;
+        if (dime_cnt == 1)
+            cout << " dime.";
+        else
+            cout << " dimes.";
+        cout << endl;
+    }
+    if (quarter_cnt > 0) {
+        cout << "You have " << quarter_cnt;
+        if (quarter_cnt == 1)
+            cout << " quarter.";
+        else
+            cout << " quarters.";
+        cout << endl;
+    }
+    if (half_dollar_cnt > 0) {
+        cout << "You have " << half_dollar_cnt;
+        if (half_dollar_cnt == 1)
+            cout << " half dollar.";
+        else
+            cout << " half dollars.";
+        cout << endl;
+    }
+    if (one_dollar_cnt > 0) {
+        cout << "You have " << one_dollar_cnt;
+        if (one_dollar_cnt == 1)
+            cout << " one dollar.";
+        else
+            cout << " one dollars.";
+        cout << endl;
+    }
+    cout << "The value of all of your coins is " << total_cent;
+    cout << " cents" << endl;
 }
