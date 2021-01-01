b8618f55c3e156548d273fb81d1213dfa664294d
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-01-01 23:52:08 +0900

    Add lines to support the conversion from yuan to dollars

--- chap04/try02_currency_converter.cpp
+++ chap04/try02_currency_converter.cpp
@@ -4,10 +4,11 @@ int main() {
     constexpr double yen_per_dollar = 103.02;
     constexpr double kroner_per_dollar = 8.59;
     constexpr double pound_per_dollar = 0.74;
+    constexpr double yuan_per_dollar = 6.53;
     double price = 0.0;
     char unit = 'a';
 
-    cout << "Please enter a price followed by a unit (y, k, or p):" << endl;
+    cout << "Please enter a price followed by a unit (y, k, p, or u):" << endl;
     cin >> price >> unit;
     
     switch (unit) {
@@ -19,6 +20,9 @@ int main() {
         break;
     case 'p':
         cout << price << " pounds == " << price / pound_per_dollar << " USD";
+        break;
+    case 'u':
+        cout << price << " yuan == " << price / yuan_per_dollar << " USD";
         break;
     default:
         cout << "Sorry, this program does not know the unit called " << unit;
