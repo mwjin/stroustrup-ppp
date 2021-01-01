4090fa7b4da87b1b0f39af14dd0816a108fbfe97
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-01-01 23:48:28 +0900

    Rewrite the currency converter program using the switch statement as instructed in 109p

---
+++ chap04/try02_currency_converter.cpp
@@ -0,0 +1,28 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+    constexpr double yen_per_dollar = 103.02;
+    constexpr double kroner_per_dollar = 8.59;
+    constexpr double pound_per_dollar = 0.74;
+    double price = 0.0;
+    char unit = 'a';
+
+    cout << "Please enter a price followed by a unit (y, k, or p):" << endl;
+    cin >> price >> unit;
+    
+    switch (unit) {
+    case 'y':
+        cout << price << " yen == " << price / yen_per_dollar << " USD";
+        break;
+    case 'k':
+        cout << price << " kroner == " << price / kroner_per_dollar << " USD";
+        break;
+    case 'p':
+        cout << price << " pounds == " << price / pound_per_dollar << " USD";
+        break;
+    default:
+        cout << "Sorry, this program does not know the unit called " << unit;
+        break;
+    }
+    cout << endl;
+}
