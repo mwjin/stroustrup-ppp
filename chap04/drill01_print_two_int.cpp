35c885588d7f5babacd4eafb559b9ef232a68507
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-01-18 22:52:17 +0900

    Do the drill 01 of the chapter 04

---
+++ chap04/drill01_print_two_int.cpp
@@ -0,0 +1,12 @@
+#include "../std_lib_facilities.h"
+
+
+int main() {
+    int num1 = 0;
+    int num2 = 0;
+
+    while (cin >> num1 >> num2) {
+        cout << "First integer: " << num1 << endl;
+        cout << "Second integer: " << num2 << endl;
+    }
+}
